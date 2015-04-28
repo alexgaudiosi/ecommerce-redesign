cf920efcf9d9d2502be2e34049a7331d2ff99580
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2015-04-28 23:28:30 +0100

    animations added

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,54 @@
 console.log('init');
 
-$('.landing-images #left-box').click(function(){
+new WOW().init();
+
+
+$('#left-box').click(function(){
 	console.log('left');
+	customKano();
 	
 });
 
 $('#center-box').click(function(){
-	console.log('center');	
+	console.log('center');
+	buyKano();
 	
 });
 
 $('#right-box').click(function(){
 	console.log('right');	
-	
+	accessories();
 });
+	
 
-$('#left-box #tester').click(function(){
-	console.log('test');	
-	
-});
 
-$('.landing-images').click(function(){
-	console.log('example');	
-	
-});
+function accessories(){
+
+	$('#left-box').addClass('fadeOutDown');
+	$('#center-box').addClass('fadeOutDown');	
+	$('#right-box').delay(1000).animate({ "margin-left": "-=450px", backgroundColor: "white"}, 'slow' );
+	$('#right-box h4').delay(1000).animate({color:"black"});
+	returnShow();
+}
+
+function buyKano(){
+	$('#left-box').addClass('fadeOutDown');
+	$('#right-box').addClass('fadeOutDown');
+	$('#center-box').delay(1000).animate({backgroundColor:"white"});
+	$('#center-box h4').delay(1000).animate({color:"black"});
+	returnShow();
+}
+
+function customKano(){
+	$('#center-box').addClass('fadeOutDown');
+	$('#right-box').addClass('fadeOutDown');
+	$('#left-box').delay(1000).animate({ "margin-left": "+=450px", backgroundColor: "white"}, 'slow' );
+	$('#left-box h4').delay(1000).animate({color:"black"});
+	returnShow();	
+}
+
+function returnShow(){
+	$('#return').fadeIn('slow');	
+	console.log('fade');
+}
+
