36c104075a55bf60a92ada6e67bf021c6f7e582a
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2015-04-28 17:32:25 +0100

    wow.js anims added

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+console.log('init');
+
+$('.landing-images #left-box').click(function(){
+	console.log('left');
+	
+});
+
+$('#center-box').click(function(){
+	console.log('center');	
+	
+});
+
+$('#right-box').click(function(){
+	console.log('right');	
+	
+});
+
+$('#left-box #tester').click(function(){
+	console.log('test');	
+	
+});
+
+$('.landing-images').click(function(){
+	console.log('example');	
+	
+});
